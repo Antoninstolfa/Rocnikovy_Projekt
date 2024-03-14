088959359f4edaddb9e0a30a0fffba451d2e6968
Antoninstolfa <97514759+Antoninstolfa@users.noreply.github.com>
2024-03-14 22:25:52 +0100

    tlačítko
    Zapojení a naprogramování tlačítka pro výpis Max rychlosti.
    
    PS:HOTOVOOO!!!

--- rocnikovy_projekt1/rocnikovy_projekt1.ino
+++ rocnikovy_projekt1/rocnikovy_projekt1.ino
@@ -3,6 +3,7 @@
 #include <Max72xxPanel.h>
 
 int pinCS = 10;
+int button1 = 5;;
 
 #define pinVcc1 4
 #define pinOut1 3
@@ -48,6 +49,8 @@ void setup() {
   pinMode(pinOut2, INPUT);
   pinMode(pinGnd2, OUTPUT);
 
+  pinMode(button1, INPUT_PULLUP);
+
   // nastavení napájecích pinů pro správné napájení modulu
   digitalWrite(pinGnd1, LOW);
   digitalWrite(pinVcc1, HIGH);
@@ -87,6 +90,24 @@ void loop() {
   // kdy 1 znamená detekovaný paprsek
   stav1 = digitalRead(pinOut1);
   stav2 = digitalRead(pinOut2);
+  int button1State = digitalRead(button1);
+
+  if(button1State == LOW)
+  {
+    String vypisMax = String(MAX_rychlost, 1); // Zaokrouhlení rychlosti na jedno desetinné místo
+    matrix.fillScreen(LOW);
+
+    int x = 1; // pozice nalevo
+    int y = (matrix.height() - 8) / 2; // Vertikální střed
+
+    for (int letter = 0; letter < vypisMax.length(); letter++) {
+      matrix.drawChar(x, y, vypisMax[letter], HIGH, LOW, 1);
+      x += width;
+    }
+
+    matrix.write();
+    delay(3000);
+  }
 
   if(casZacatek > 0 && casKonec > 0 && zmereno == true)
   {
